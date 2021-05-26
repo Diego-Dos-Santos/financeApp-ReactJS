0af6b719e92abe406ea95f7f88d83b7c51b244e0
Diego Dos Santos <diegodossantos@macbook-pro.home>
2021-05-26 14:06:17 +0200

    finance app ended

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
-import Pregunta from "./components/Pregunta"
+import React, { useState, useEffect } from 'react';
+import Pregunta from "./components/Pregunta";
+import Formulario from "./components/Formulario";
+import Listado from "./components/Listado";
+import ControlPresupuesto from "./components/ControlPresupuesto";
 
 function App() {
 
@@ -7,19 +10,71 @@ function App() {
 
   const [presupuesto, guardarPresupuesto] = useState(0);
   const [restante, guardarRestante] = useState(0);
+  const [mostrarpregunta, actualizarPregunta] = useState(true);
+  const [gasto, guardaGasto] = useState({});
+  const [creargasto, guardarCrearGasto] = useState(false);
+
+  //crear un listado de gastos
+
+  const [gastos, guardaGastos] = useState([]);
+
+  // useEffect que actualiza el restante
+
+  useEffect(() => {
+    if(creargasto){
+
+      //agrega el nuevo presupuesto
+      guardaGastos([
+        ...gastos,
+        gasto
+      ])
+
+      // resta del presupuesto actual
+
+      const presupuestoRestante = restante - gasto.cantidad;
+      guardarRestante(presupuestoRestante);
+      
+      // Resetear a false 
+      guardarCrearGasto(false);
+    }
+  }, [gasto, creargasto, gastos, restante]);
 
   return (
     <div className="container">
         <header>
           <h1>Gastos Semanales</h1>
-        </header>
 
-        <div className="contenido-principal contenido">
-          <Pregunta 
-            guardarPresupuesto={guardarPresupuesto}
-            guardarRestante={guardarRestante} 
-          />
-        </div>
+          <div className="contenido-principal contenido">
+        
+            { mostrarpregunta ? 
+            ( 
+              <Pregunta 
+                guardarPresupuesto={guardarPresupuesto}
+                guardarRestante={guardarRestante}
+                actualizarPregunta={actualizarPregunta}
+              />
+            ) : (
+              <div className="row">
+                <div className="one-half column">
+                  <Formulario 
+                    guardaGasto={guardaGasto}
+                    guardarCrearGasto={guardarCrearGasto}
+                  />
+                </div>
+                <div className="one-half column">
+                  <Listado 
+                    gastos={gastos}
+                  />
+
+                  <ControlPresupuesto 
+                    presupuesto={presupuesto}
+                    restante={restante}
+                  />
+                </div>
+              </div>
+            ) }     
+          </div>
+        </header>
     </div>
   );
 }
--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,7 +1,8 @@
 import React, { Fragment, useState } from 'react';
+import PropTypes from 'prop-types';
 import Error from './Error';
 
-const Pregunta = ({ guardarPresupuesto, guardarRestante }) => {
+const Pregunta = ({ guardarPresupuesto, guardarRestante, actualizarPregunta }) => {
 
     // state 
 
@@ -31,6 +32,7 @@ const Pregunta = ({ guardarPresupuesto, guardarRestante }) => {
         guardarError(false);
         guardarPresupuesto(cantidad);
         guardarRestante(cantidad);
+        actualizarPregunta(false)
     }
 
 
@@ -58,6 +60,12 @@ const Pregunta = ({ guardarPresupuesto, guardarRestante }) => {
         </form>
        </Fragment>
      );
+}
+
+Pregunta.propTypes = {
+    guardarPresupuesto: PropTypes.func.isRequired,
+    guardarRestante: PropTypes.func.isRequired,
+    actualizarPregunta: PropTypes.func.isRequired
 }
  
 export default Pregunta;
